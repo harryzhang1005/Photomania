be4736038e04266de02942dec37b0dc27d0f32cb
Harvey <zhanghfglad@gmail.com>
2015-12-16 14:09:05 -0500

    Initial Commit

---
+++ Photomania/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Photomania
+//
+//  Created by Harvey Zhang on 12/16/15.
+//  Copyright © 2015 HappyGuy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
